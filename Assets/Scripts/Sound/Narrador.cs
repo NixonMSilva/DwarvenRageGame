efc6a8f5cb8c798c26eb640d15eb07927e3cd791
Lfseibel <43753872+Lfseibel@users.noreply.github.com>
2021-07-16 02:34:57 -0400

    Falas?
    Falas?

--- Assets/Scripts/Sound/Narrador.cs
+++ Assets/Scripts/Sound/Narrador.cs
@@ -1,24 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Narrador : MonoBehaviour
 {
     [SerializeField] GameObject video1;
-
-    GameObject video2;
+    
+    TestPassLevel passLevel;
+    [SerializeField] GameObject video2;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
         StartCoroutine(waiter());
-
-        
+   
     }
 
     IEnumerator waiter()
 {
-
+    AudioManager.instance.PlaySound("fireplace");
     AudioManager.instance.PlaySound("Narrador");
 
    
@@ -26,6 +27,7 @@ public class Narrador : MonoBehaviour
     
     video1.SetActive(false);
 
+    video2.SetActive(true);
     AudioManager.instance.PlaySound("narrador1");
 
     //Wait for 2 seconds
@@ -36,7 +38,16 @@ public class Narrador : MonoBehaviour
     yield return new WaitForSeconds(15);   
 
     AudioManager.instance.PlaySound("narrador3"); 
+
+    yield return new WaitForSeconds(22); 
+
+    AudioManager.instance.DestroyAllSounds();
+
+    passLevel.Jogo();
+    
 }
+
+
 
     // Update is called once per frame
 }
