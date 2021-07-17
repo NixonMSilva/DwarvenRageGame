f5302f7f7204d93b86c8cce281d003f3acec2d7e
Nixon Silva <nixonmoreira@unifei.edu.br>
2021-07-17 01:44:16 -0300

    Misc Fixes
    Options sliders permanency fix and skybox fix on the narrator's scene.

--- Assets/Scripts/UI/OptionsController.cs
+++ Assets/Scripts/UI/OptionsController.cs
@@ -10,19 +10,30 @@ public class OptionsController : MonoBehaviour
     [SerializeField] private Slider sslider;
 
     [SerializeField] private Slider vmslider;
-    
+
+    public void Start ()
+    {
+        // Changes the sliders to their values based on global variables
+        vslider.value = GameManager.instance.sfxVolume;
+        sslider.value = InputHandler.instance.mouseSensitivity;
+        vmslider.value = AudioManager.instance.MusicVolume;
+    }
+
     public void ChangeVolume ()
     {
+        // Change the volume for sound effects
         GameManager.instance.sfxVolume = vslider.value;
     }
 
     public void ChangeSensitivity ()
     {
+        // Change the mouse look sensitivity
         InputHandler.instance.mouseSensitivity = sslider.value;
     }
 
     public void ChangeMusicVolume ()
     {
+        // Change the volume for music
         AudioManager.instance.MusicVolume = vmslider.value;
     }
 }
